1e31025e96ad67dee73f7ee362d91ecde38fa6e9
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-04 20:54:31 -0500

    feat: :sparkles: Tabla photos
    Se creo la tabla photos y se lleno con los datos de la seccion photos de la api

--- app.js
+++ app.js
@@ -117,6 +117,46 @@ let server = createServer((req, res)=>{
                 res.end(plantilla);
         })
         })
+    }else if(req.url =="/photos"){
+        https.get('https://jsonplaceholder.typicode.com/photos',(data)=>{
+    
+        data.on("data", (chunk)=>{
+            json+=chunk;
+        })
+        data.on("end",()=>{
+            let plantilla=/* html */
+            `<table border="solid">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>URL</th>
+                        <th>THUMBNAILURL</th>
+                    </tr>
+                </thead>
+                <tbody>`
+            ;
+            json= JSON.parse(json);
+            json.forEach((val,id)=>{
+                plantilla+=/* html */
+                `<tr>
+                    <td>${val.albumId}</td>
+                    <td>${val.id}</td>
+                    <td>${val.title}</td>
+                    <td>${val.url}</td>
+                    <td>${val.thumbnailUrl}</td>
+                </tr>
+                `
+                
+            });
+            plantilla+=/* html */`
+                    </tbody>
+                </table>`;
+                res.end(plantilla);
+        })
+        })
+    
     }
     
 })
