13da231d6a28aae7a4862b5e3640de77c9202315
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-04 20:55:06 -0500

    feat: :sparkles: tabla todos
    Se creo la tabla todos y se lleno con los datos de la api

--- app.js
+++ app.js
@@ -156,7 +156,43 @@ let server = createServer((req, res)=>{
                 res.end(plantilla);
         })
         })
+    }else if(req.url =="/todos"){
+        https.get('https://jsonplaceholder.typicode.com/todos',(data)=>{
     
+        data.on("data", (chunk)=>{
+            json+=chunk;
+        })
+        data.on("end",()=>{
+            let plantilla=/* html */
+            `<table border="solid">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>COMPLETED</th>
+                    </tr>
+                </thead>
+                <tbody>`
+            ;
+            json= JSON.parse(json);
+            json.forEach((val,id)=>{
+                plantilla+=/* html */
+                `<tr>
+                    <td>${val.userId}</td>
+                    <td>${val.id}</td>
+                    <td>${val.title}</td>
+                    <td>${val.completed}</td>
+                </tr>
+                `
+                
+            });
+            plantilla+=/* html */`
+                    </tbody>
+                </table>`;
+                res.end(plantilla);
+        })
+        })
     }
     
 })
